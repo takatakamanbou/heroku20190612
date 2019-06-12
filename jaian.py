c45c1046449319077da2263ba88ed0db86bd7ed0
TAKAHASHI <takatakamanbou@gmail.com>
2019-06-12 21:20:07 +0900

    20190612

---
+++ jaian.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import os
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+
+@app.route('/janken/')
+def janken():
+    r = np.random.random()
+    if r < 1/3:
+        return 'ぐう'
+    elif r < 2/3:
+        return 'ちょき〜ん'
+    else:
+        return 'ぱぁ'
+
+
+@app.route('/user/')
+def user():
+    return os.environ['USER']
+
+
+if __name__ == '__main__':
+    app.run()
